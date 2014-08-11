4f855f910af5be76400b27bf878dc97c30acedc8
Don <doncarver@hotmail.com>
2014-08-11 08:33:54 -0700

    New package for bugs.  Bug class names should reference bug number but it currently doesn't exist.

---
+++ src/com/selenium/wikitest/bugs/SearchError.java
@@ -0,0 +1,44 @@
+package com.selenium.wikitest.bugs;
+
+import com.selenium.wikitest.webpage.homepage.HomePage;
+import com.selenium.wikitest.webpage.homepage.HomePageText;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.AfterClass;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.thoughtworks.selenium.SeleneseTestBase;
+
+public class SearchError {
+	
+	private static HomePage homePage = new HomePage(new FirefoxDriver());
+	
+	@Before
+	public void setupTest() {
+		homePage.openPage();
+	}
+	
+	@Test
+	public void testFailSearch() throws Exception {
+		// Set expected outcome
+		//String errorExpected = HomePageText.getString("ErrorPage.NotFoundTitle");
+
+		// Set search text
+		String searchText = HomePageText.getString("HomePage.OpenBraceSearch");
+		
+		// Run search and get result
+		homePage.searchFor(searchText);
+		
+		// Verify a meaningless error is not displayed (this assert fails)
+		// Make a separate bug test
+		String searchFail = homePage.getTextAtXPath("ErrorPage.ErrorXPath");
+		SeleneseTestBase.assertFalse(searchFail.contains(HomePageText.getString("ErrorPage.SearchErrored")));
+	}
+	
+	@AfterClass
+	public static void commonTearDown() throws Exception {
+		homePage.closeBrowser();
+	}
+
+}
