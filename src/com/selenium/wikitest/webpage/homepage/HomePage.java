7448337e0d5c5882ff3b3c7b0ca6d03444fb083f
Don <doncarver@hotmail.com>
2014-08-20 07:49:30 -0700

    Dramatically simplified isLanguageFound() method.  Thanks for the help Jason.

--- src/com/selenium/wikitest/webpage/homepage/HomePage.java
+++ src/com/selenium/wikitest/webpage/homepage/HomePage.java
@@ -22,27 +22,6 @@ public class HomePage extends WebPage{
 		Portuguese
 	}
 	
-	private static final String expectedEnglishDisplay = "HomePage.LanguageEnglishText";
-	private static final String actualEnglishDisplay = "HomePage.LanguageXPathEnglish";
-	private static final String expectedSpanishDisplay = "HomePage.LanguageSpanishText";
-	private static final String actualSpanishDisplay = "HomePage.LanguageXPathSpanish";
-	private static final String expectedJapaneseDisplay = "HomePage.LanguageJapaneseText";
-	private static final String actualJapaneseDisplay = "HomePage.LanguageXPathJapanese";
-	private static final String expectedRussianDisplay = "HomePage.LanguageRussianText";
-	private static final String actualRussianDisplay = "HomePage.LanguageXPathRussian";
-	private static final String expectedFrenchDisplay = "HomePage.LanguageFrenchText";
-	private static final String actualFrenchDisplay = "HomePage.LanguageXPathFrench";
-	private static final String expectedGermanDisplay = "HomePage.LanguageGermanText";
-	private static final String actualGermanDisplay = "HomePage.LanguageXPathGerman";
-	private static final String expectedItalianDisplay = "HomePage.LanguageItalianText";
-	private static final String actualItalianDisplay = "HomePage.LanguageXPathItalian";
-	private static final String expectedChineseDisplay = "HomePage.LanguageChineseText";
-	private static final String actualChineseDisplay = "HomePage.LanguageXPathChinese";
-	private static final String expectedPolishDisplay = "HomePage.LanguagePolishText";
-	private static final String actualPolishDisplay = "HomePage.LanguageXPathPolish";
-	private static final String expectedPortugueseDisplay = "HomePage.LanguagePortugueseText";
-	private static final String actualPortugueseDisplay = "HomePage.LanguageXPathPortuguese";
-	
 	public HomePage(WebDriver driver) {
 		super(driver);
 	}
@@ -68,70 +47,9 @@ public class HomePage extends WebPage{
 	}
 	
 	public boolean isLanguageFound(Language val) {
+				
+		return getDisplayedLanguage("HomePage.LanguageXPath" + val.toString()).contains(getExpectedLanguage("HomePage.Language" + val.toString() + "Text"));
 		
-		String propertyExpected = null;
-		String propertyActual = null;
-		
-		switch (val) {
-			case English:
-				propertyExpected = expectedEnglishDisplay;
-				propertyActual = actualEnglishDisplay;
-				break;
-				
-			case Spanish:
-				propertyExpected = expectedSpanishDisplay;
-				propertyActual = actualSpanishDisplay;
-				break;
-				
-			case Japanese:
-				propertyExpected = expectedJapaneseDisplay;
-				propertyActual = actualJapaneseDisplay;
-				break;
-				
-			case Russian:
-				propertyExpected = expectedRussianDisplay;
-				propertyActual = actualRussianDisplay;
-				break;
-				
-			case French:
-				propertyExpected = expectedFrenchDisplay;
-				propertyActual = actualFrenchDisplay;
-				break;
-				
-			case German:
-				propertyExpected = expectedGermanDisplay;
-				propertyActual = actualGermanDisplay;
-				break;
-				
-			case Italian:
-				propertyExpected = expectedItalianDisplay;
-				propertyActual = actualItalianDisplay;
-				break;
-				
-			case Chinese:
-				propertyExpected = expectedChineseDisplay;
-				propertyActual = actualChineseDisplay;
-				break;
-				
-			case Polish:
-				propertyExpected = expectedPolishDisplay;
-				propertyActual = actualPolishDisplay;
-				break;
-				
-			case Portuguese:
-				propertyExpected = expectedPortugueseDisplay;
-				propertyActual = actualPortugueseDisplay;
-				break;
-			
-			default:
-				break;
-			
-		}
-		
-		String expected = getExpectedLanguage(propertyExpected);
-		String actual = getDisplayedLanguage(propertyActual);
-		
-		return actual.contains(expected);
 	}
 	
 }
